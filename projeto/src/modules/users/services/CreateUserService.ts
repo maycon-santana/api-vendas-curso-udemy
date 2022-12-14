03ff8d92d713a288598f75cab84ef287b1c2d75b
maycon-santana <maycon.santana@thstec.com.br>
2022-12-13 22:48:29 -0300

    Instalação das dependências yarn add bcryptjs e yarn add -D @types/bcryptjs para Criptografia de senha

--- projeto/src/modules/users/services/CreateUserService.ts
+++ projeto/src/modules/users/services/CreateUserService.ts
@@ -1,34 +1,37 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 
 interface IRequest {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 class CreateUserService {
-    public async execute({ name, email, password }: IRequest): Promise<User> {
-        const usersRepository = getCustomRepository(UsersRepository);
-        const emailExists = await usersRepository.findByEmail(email);
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
 
-        if (emailExists) {
-            throw new AppError('E-mail address already used.');
-        }
-
-        const user = usersRepository.create({
-            name,
-            email,
-            password,
-        });
-
-        await usersRepository.save(user);
-
-        return user;
+    if (emailExists) {
+      throw new AppError('E-mail address already used.');
     }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
 }
 
 export default CreateUserService;
